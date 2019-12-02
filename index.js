227863f01e35a4de7af6a78a85938a53988cd29c
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-12-01 17:04:14 -0700

    add functionality to flip a card

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
 let cardHolder = document.getElementById("card-holder");
 let cards = [];
+let cardId = 0;
 let createNewCardBtn = document.getElementById("new-card-btn");
 
 const createNewCard = () => {
   let card = {
-    title: "Test"
+    back: "Answer",
+    display: "Question",
+    front: "Question",
+    id: cardId,
+    isfront: true
   };
+  cardId += 1;
   cards.push(card);
   renderCards();
 }
 
 const renderCards = () => {
-  let text = ""
+  cardHolder.innerHTML = "";
+  
   for(i in cards) {
-    text += `<div class="card"> ${cards[i].title} </div>`;
+    renderCard(cards[i]);
   }
-  cardHolder.innerHTML = text;
+
+}
+
+const renderCard = (card) => {
+  let attr = document.createAttribute("class");
+  attr.value = "card";
+
+  let flipCardbtn = document.createElement("BUTTON");
+  flipCardbtn.innerHTML = "Flip Card";
+  flipCardbtn.addEventListener("click", function(){
+    flipCard(card);
+  });
+
+  let cardTag = document.createElement("DIV");
+  cardTag.setAttributeNode(attr);
+  cardTag.innerHTML = card.display;
+  cardTag.appendChild(flipCardbtn);
+
+  cardHolder.appendChild(cardTag);
+}
+
+const flipCard = (card) => {
+  if (card.isfront === true) {
+    card.display = card.back
+    card.isfront = false
+  } else {
+    card.display = card.front
+    card.isfront = true
+  }
+  renderCards();
+}
+
+const removeCard = (card) => {
+  
 }
 
 createNewCardBtn.addEventListener("click", createNewCard);
